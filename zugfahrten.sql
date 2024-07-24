0742866ed6e72c026f4beffb4fc1a9738e86396a
Brigitte Kupka <brigitte.kupka@gmx.at>
2024-07-24 14:40:17 +0200

    Completed SQL Queries - Aufgabe 4
    Former-commit-id: 76a0e0ed2d5cd06613923ec593ace04db6cb3c7f

--- zugfahrten.sql
+++ zugfahrten.sql
@@ -2,6 +2,8 @@
 select count(distinct z.Fahrtnummer) from zugfahrten z;
 
 -- Welche Zugfahrt hat die meisten Fahrzeugnummern?
+-- Edit: Nach Bereinigung, manche Zeilen wurden wegen fehlender Werte in anderen Spalten entfernt
+
 select f.Fahrtnummer, max(anz) from
 (select z.Fahrtnummer , count(z.Fahrzeugnummer) anz 
 from zugfahrten z group by z.Fahrtnummer
@@ -17,8 +19,10 @@ group by jahr order by jahr
 -- Frühestes Abfahrtsdatum und Durchschnitt der Messung der Zugfahrten der jeweiligen Buchungskreise
 -- Frühestes Datum als Text? max und min funktioniert auch auf den string
 -- TODO Durchschnitt pro Fahrt und dann pro Buchungskreis oder Durchschnitt über alle Fahrten pro Buchungskreis?
-select z.Buchungskreis , avg(z.messung) from zugfahrten z 
-group by z.Buchungskreis
+select z.Buchungskreis , min(z.abfahrtsdatum), avg(z.messung) 
+from zugfahrten z 
+group by z.Buchungskreis 
+
 
 -- Welcher Buchungskreis hat die höchste Gesamtsumme bei den Zugfahrten in den jeweiligen Jahren?
 SELECT max(anz.anz_fahrten), anz.Buchungskreis from 
@@ -30,14 +34,25 @@ group by z.Buchungskreis ) anz
 select Fahrtnummer , Buchungskreis , max(Abfahrtszeit) from zugfahrten z
 group by Buchungskreis 
 
-select z.Fahrtnummer, z.Abfahrtsdatum, z.Fahrzeugnummer, row_number() over (partition by z.Fahrtnummer) 
+-- Bei den verschiedenen Buchungskreisen wird von der aktuellsten Zugfahrt die Fahrzeugnummer mit der zweithöchsten Messung gesucht 
+-- Ranks zu Messungen 
+select m_ranks.Buchungskreis, m_ranks.Fahrzeugnummer
+from (
+select z.Buchungskreis , z.Abfahrtsdatum, z.Fahrtnummer, z.Fahrzeugnummer, z.Messung, 
+		rank() over (partition by z.Fahrtnummer order by Messung desc) as messung_rank 
 from zugfahrten z 
-order by z.Fahrtnummer , z.Abfahrtsdatum 
+order by z.Buchungskreis, z.Fahrtnummer,  messung_rank  asc) m_ranks
+where m_ranks.messung_rank = 2
+group by m_ranks.Buchungskreis
+having max(m_ranks.Abfahrtsdatum)
 
 
+
+---- Checks 
+-- Vergleiche Abfahrtsdatum mit dem Datum in der Süalte Abfahrtszeit
 select Fahrtnummer, Fahrzeugnummer, SUBSTR(Abfahrtsdatum,1,10) datum, SUBSTR(Abfahrtszeit,1,10) zeit from zugfahrten z
 where SUBSTR(Abfahrtsdatum,1,10) <> SUBSTR(Abfahrtszeit,1,10)   
 
-
+-- Ist zug id immer gleich fahrtnummer?
 select * from zugfahrten z 
 where z."Zug id" == z.Fahrtnummer 
