ef68a48bd01e523125df4bb7144ac911c913377e
Brigitte Kupka <brigitte.kupka@gmx.at>
2024-07-25 16:12:24 +0200

    cleanup, added outlier analyses
    Former-commit-id: 6fba6f4fc6d7ab01cf07ab0759ad548421719be6

--- zugfahrten.py
+++ zugfahrten.py
@@ -1,153 +1,174 @@
+##########################################################
+### Technisches Assessment - Data Engineer / ETL
+### Brigitte Kupka
+### brigitte.kupka@gmx.at
+##########################################################
+
 import pandas as pd
-import numpy as np
 import sqlite3 
+import sys
 import os
-import logging
 
 def log_invalid_rows(reason):
   null_mask = df_temp.isnull().any(axis=1)
-  null_rows = df[null_mask]
+  null_rows = df.loc[null_mask]
   null_rows = null_rows.assign(grund=reason)
   if null_rows.shape[0] > 0:
     null_rows.to_csv('zugfahrten_errors.csv',mode='a', header=False)
 
 
-
-
-
-logging.basicConfig(
-    filename="zugfahrten.log",
-    encoding="utf-8",
-    filemode="a",
-    format="{asctime} - {levelname} - {message}",
-    style="{",
-    datefmt="%Y-%m-%d %H:%M", 
-    level=logging.INFO)
-
-logging.info('Loading zugfahrten.csv')
-
-os.chdir('C:/Users/morsx/Documents/docs-archive/Bewerbung 2024/ÖBB IT Lösungsentwickler Schwerpunkt Data Engineering/Technisches Assessment')
+#####################################################
+### 1 - EXTRACT #####################################
+#####################################################
+os.chdir(sys.path[0])
 df = pd.read_csv('zugfahrten.csv')
-logging.info('Rows found in file: %d', df.shape[0])
-logging.info('Begin validation of file')
 
-#Delete duplicates
-logging.info('Looking for duplicate rows')
+#####################################################
+### 2 - TRANSFORM ###################################
+#####################################################
+###
+### Delete duplicates and write duplicate rows to csv log
+###
 df_duplicates = df[df.duplicated(keep='first')]
-df_duplicates.sort_values(by=['Fahrtnummer', 'Fahrzeugnummer'])
-logging.info('Number of duplicates found: %d', df_duplicates.shape[0])
 df_duplicates = df_duplicates.assign(grund='Duplicate')
 df_duplicates.to_csv('zugfahrten_errors.csv',mode='a')
-
 df.drop_duplicates(inplace=True)
-logging.info('Remaining rows after removing duplicates: %d', df.shape[0])
 
-# Removing rows with missing values
-logging.info('Looking for rows with missing values')
+###
+### Remove rows with missing values 
+###
+# To be able to log the deleted rows with reason for deletion,
+# a boolean mask is created to find the to be deleted rows in the dataframe, mark them and write them to csv log
+# only after logging, the rows are dropped from the original dataframe
+
 null_mask = df.isnull().any(axis=1)
 null_rows = df[null_mask]
 null_rows = null_rows.assign(grund='Missing Values')
-logging.info('Number of rows with missing values found: %d', null_rows.shape[0])
-
 if null_rows.shape[0] >0:
     null_rows.to_csv('zugfahrten_errors.csv',mode='a', header=False)
-# Deleting rows with empty cells
 df = df.dropna()
-logging.info('Remaining rows after removing missing values: %d', df.shape[0])
  
-logging.info('Converting Columns to appropriate data types and filtering out illegal values')
-logging.info('Abfahrtsdatum')
-logging.info('Zeilen vor Umwandeln Abfahrtsdatum: %d', df.shape[0])
-#df_temp is needed so that I can write the unchanged rows to the csv-log.
-#df only has empty cells where a value could not be converted
+###
+### Convert datatypes in dataframe
+###
+#
+# Attempting to convert Abfahrtsdatum to datetime
+# df_temp is needed so that I can write the unchanged rows to the csv-log.
+# since null values were already removed in the previous step, 
+# df now only has empty cells where a value could not be converted
+# mark the rows and save them to the csv log
 df_temp = df
 df_temp['Abfahrtsdatum'] = pd.to_datetime(df['Abfahrtsdatum'], format='mixed')
 log_invalid_rows('Abfahrtsdatum could not be converted to datetime')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Abfahrtsdatum: %d', df.shape[0])
 
-logging.info('Date 01.01.1900 will be treated as illegal date and removed')
+# Date 01.01.1900 will be treated as illegal date
+# The rows are written to the csv log and removed from df
 illegal_date_rows = df[df["Abfahrtsdatum"] ==pd.to_datetime('1900-01-01')]
 illegal_date_rows = illegal_date_rows.assign(grund='Abfahrtsdatum was 01.01.1900')
 illegal_date_rows.to_csv('zugfahrten_errors.csv',mode='a', header=False)
 df.drop(df.loc[df['Abfahrtsdatum']==pd.to_datetime('1900-01-01')].index, inplace=True)
-logging.info('Rows after removing 01.01.1900: %d', df.shape[0])
 
-logging.info('Abfahrtszeit')
-logging.info('Zeilen vor Umwandeln Abfahrtszeit: %d', df.shape[0])
+# angenommenes Datum des Exports 23.07.2024
+# Abfahrtsdatum in der Zukunft wird entfernt
+illegal_date_rows = df[df["Abfahrtsdatum"] >pd.to_datetime('2024-07-23')]
+illegal_date_rows = illegal_date_rows.assign(grund='Abfahrtsdatum was in the future')
+illegal_date_rows.to_csv('zugfahrten_errors.csv',mode='a', header=False)
+df.drop(df.loc[df['Abfahrtsdatum']>pd.to_datetime('2024-07-23')].index, inplace=True)
+
+# Versuche Abfahrtszeit in datetime umzuwandeln
+# Anmerkung: Abfahrtszeit enthält ein Datum, das vom Abfahrtsdatum stark abweicht
+# Ich nehme an, das ist Prozessbedingt und nicht relevant
+# Umwandeln in datetime, ungültige Zeilen loggen, leere Zeilen löschen, wo die Konvertierung fehlgeschlagen ist
 df_temp['Abfahrtszeit'] = pd.to_datetime(df['Abfahrtszeit'], format='mixed', errors='coerce')    
 log_invalid_rows('Abfahrtszeit could not be converted to datetime')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Abfahrtszeit: %d', df.shape[0])
+# Abfahrtszeit in der Zukunft wird entfernt
+# Obwohl das Datum in Abfahrtszeit und Ankunftszeit eigentlich nicht verwendet ist, gibt es
+# zumindest einen fehlerhaften Eintrag mit Datum in der Zukunft, der eine falsche Reisezeit ergibt
+# Wird daher entfernt
+illegal_date_rows = df[df["Abfahrtszeit"] >pd.to_datetime('2024-07-23')]
+illegal_date_rows = illegal_date_rows.assign(grund='Abfahrtszeit was in the future')
+illegal_date_rows.to_csv('zugfahrten_errors.csv',mode='a', header=False)
+df.drop(df.loc[df['Abfahrtszeit']>pd.to_datetime('2024-07-23')].index, inplace=True)
 
-logging.info('Ankunftszeit')
-logging.info('Zeilen vor Umwandeln Ankunftszeit: %d', df.shape[0])
+# Versuche Abfahrtszeit in datetime umzuwandeln - same as above
 df_temp['Ankunftszeit'] = pd.to_datetime(df['Ankunftszeit'], format='mixed', errors='coerce')
 log_invalid_rows('Ankunftszeit could not be converted to datetime')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Abfahrtszeit: %d', df.shape[0])
 
-logging.info('Zug id')
-logging.info('Zeilen vor Umwandeln Zug id: %d', df.shape[0])
+# Entferne Abfahrstzeit in der Zukunft - siehe Anmerkung oben
+illegal_date_rows = df[df["Ankunftszeit"] >pd.to_datetime('2024-07-23')]
+illegal_date_rows = illegal_date_rows.assign(grund='Ankunftszeit was in the future')
+illegal_date_rows.to_csv('zugfahrten_errors.csv',mode='a', header=False)
+df.drop(df.loc[df['Ankunftszeit']>pd.to_datetime('2024-07-23')].index, inplace=True)
+
+
+# Versuche Zug id in numeric umzuwandeln
+# Ungültige Zeilen werden in CSV geschrieben und anschließend gelöscht
 df_temp['Zug id'] = pd.to_numeric(df['Zug id'], errors='coerce')
 log_invalid_rows('Zug id could not be converted to numeric')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Zug id: %d', df.shape[0])
 
-logging.info('Buchungskreis')
-logging.info('Zeilen vor Umwandeln Buchungskreis: %d', df.shape[0])
+
+# Versuche Buchungskreis in numeric umzuwandeln
 df_temp['Buchungskreis'] = pd.to_numeric(df['Buchungskreis'], errors='coerce')
 log_invalid_rows('Buchungskreis could not be converted to numeric')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Buchungskreis: %d', df.shape[0])
 
-logging.info('Entfernung km')
-logging.info('Zeilen vor Umwandeln Entfernung km: %d', df.shape[0])
+# Versuche Entfernung km in numeric umzuwandeln
 df_temp['Entfernung km'] = pd.to_numeric(df['Entfernung km'], errors='coerce')
 log_invalid_rows('Entfernung km could not be converted to numeric')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Entfernung km: %d', df.shape[0])
 
-logging.info('Passagierzahl')
-logging.info('Zeilen vor Umwandeln Passagierzahl: %d', df.shape[0])
+# Versuche Passagieranzahl in numeric umzuwandeln
 df_temp['Passagieranzahl'] = pd.to_numeric(df['Passagieranzahl'], errors='coerce')
 log_invalid_rows('Passagierzahl could not be converted to numeric')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Passagierzahl: %d', df.shape[0])
 
-logging.info('Abfahrtsbahnhof')
-logging.info('Zeilen vor Umwandeln Abfahrtsbahnhof: %d', df.shape[0])
+# Versuche Abfahrtsbahnhof in String umzuwandeln
 df_temp['Abfahrtsbahnhof'] = df['Abfahrtsbahnhof'].astype(str)
 log_invalid_rows('Abfahrtsbahnhof could not be converted to string')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Abfahrtsbahnhof: %d', df.shape[0])
 
-logging.info('Ankunftsbahnhof')
-logging.info('Zeilen vor Umwandeln Ankunftsbahnhof: %d', df.shape[0])
+# Versuche Ankunftsbahnhof in String umzuwandeln
 df_temp['Ankunftsbahnhof'] = df['Ankunftsbahnhof'].astype(str)
 log_invalid_rows('Ankunftsbahnhof could not be converted to string')
 df = df_temp.dropna()
-logging.info('Zeilen nach Umwandeln Ankunftsbahnhof: %d', df.shape[0])
 
-logging.info('Starting calculations on data')
-
-logging.info('Reisezeit in Minuten')
+###
+### Berechnen zusätzlicher Information
+###
+# Reisezeit in Minuten
 df['Reisezeit'] = (df['Ankunftszeit']-df['Abfahrtszeit'])
 df['Reisezeit'] = df['Reisezeit'].dt.total_seconds()/60.0
 
-logging.info('Adding Reisezeit in Stunden')
+# Reisezeit in Stunden
 df['Reisezeit Stunden'] = df['Reisezeit'] / 60.0
 
-logging.info('Adding Durchschnittsgeschwindigkeit')
+# Durchschnittsgeschwindigkeit
+# Einige Zeilen haben eine unrealitische Durchschnittsgeschwindigkeit, wegen Kombinationen von kurzen Reisezeiten für große Distanz
+# Klären und ggf. entfernen
 df['Durchschnittsgeschwindigkeit'] = (df['Entfernung km']/df['Reisezeit Stunden'])
 
-#TODO Gesamtzahl der Passagiere pro Zug pro Tag - Abfahrtsdatum
-#gesamtzahl_df =  df.groupby(['Zug id', 'Abfahrtsdatum'])['Passagieranzahl'].sum()
 
+# Gesamtzahl der Passagiere pro Zug pro Tag - Abfahrtsdatum
+# Achtung: Abfahrtsdatum ist nicht sauber, es gibt zwischendurch anscheinend fehlerhafte Datumseinträge bei einzelnen Zeilen
+# Man sieht das gut, wenn man die Groupby Reihenfolge umgekehrt und zuerst nach Zug Id und dann nach Abfahrtsdatum Gruppiert
+# eine anderer Ansatz findet sich in zugfahrten.sql, aber eine optimale Lösung ist schwierig. Welches Datum nimmt man?
+# Das das pro Fahrt am häufigsten vorkommt? 
+gesamtzahl_df =  df.groupby(['Abfahrtsdatum','Zug id',]).Passagieranzahl.agg(['sum'])
+#print(gesamtzahl_df.to_string())
 
-# store to sqlite
+#####################################################
+### 3 - LOAD ########################################
+#####################################################
+# SLQ Lite Tabelle aus den Daten erstellen
 conn = sqlite3.connect('zugfahrten.sqlite')
 df.to_sql('zugfahrten', conn, if_exists='replace', index=False)
+
+# Dataframe nach Transformation auch für weitere Betrachtungen in neues csv schreiben
+df.to_csv('zugfahrten_transformed.csv', index=False)
 
 
 
--- zugfahrten.sql
+++ zugfahrten.sql
@@ -1,39 +1,60 @@
+------------------------------------------------------------
+--- Technisches Assessment - Data Engineer / ETL
+--- Brigitte Kupka
+--- brigitte.kupka@gmx.at
+------------------------------------------------------------
+
+------------------------------------------------------------
 -- Wieviele Zugfahrten gibt es insgesamt?
 select count(distinct z.Fahrtnummer) from zugfahrten z;
 
+------------------------------------------------------------
 -- Welche Zugfahrt hat die meisten Fahrzeugnummern?
--- Edit: Nach Bereinigung, manche Zeilen wurden wegen fehlender Werte in anderen Spalten entfernt
+-- Anmerkung: Nach Bereinigung, denn manche Zeilen wurden wegen fehlender Werte in anderen Spalten entfernt
 
 select f.Fahrtnummer, max(anz) from
 (select z.Fahrtnummer , count(z.Fahrzeugnummer) anz 
 from zugfahrten z group by z.Fahrtnummer
 ) f;
 
--- Wieviele Zugfahrten wurden in den jeweiligen Jahren durchgeführt
+------------------------------------------------------------
+-- Wieviele Zugfahrten wurden in den jeweiligen Jahren durchgeführt?
 -- Achtung, das geht davon aus, dass das Abfahrtsdatum bei jeder Zeile für eine Fahrtnummer gleich ist
--- Wenn es Fehler beim Jahr gibt, dann stimmt das hier nicht
+-- Es gibt aber Zeilen, wo anscheinend fehlerhafte Abfahrsdatum bei einzelnen Fahrzeugen vorkommen. Diese sind hier getrennt gezählt
 select count(distinct z.fahrtnummer), substr(z.Abfahrtsdatum,1,4) jahr
 from zugfahrten z 
 group by jahr order by jahr
 
+-- Etwas bessere Möglichkeit - nehme immer nur das Abfahrtsdatum vom ersten Fahrzeug, statt distinct auf fahrtnummer wie oben, dadurch fallen
+-- falsche Datumseinträge bei den Zeilen weg
+-- Einige sind trotzdem noch falsch, wenn das fehlerhafte Datum beim ersten Fahrzeug steht
+-- Eigentlich gehören diese Fahrten raus. 
+select count(fahrten.Fahrtnummer), fahrten.jahr
+from
+	(select z.fahrtnummer, substr(z.Abfahrtsdatum,1,4) jahr
+	from zugfahrten z 
+	group by z.Fahrtnummer 
+	having min(z.Fahrzeugnummer)) fahrten
+group by jahr
+order by jahr
+
+
+------------------------------------------------------------
 -- Frühestes Abfahrtsdatum und Durchschnitt der Messung der Zugfahrten der jeweiligen Buchungskreise
--- Frühestes Datum als Text? max und min funktioniert auch auf den string
--- TODO Durchschnitt pro Fahrt und dann pro Buchungskreis oder Durchschnitt über alle Fahrten pro Buchungskreis?
+-- Frühestes Datum als Text - sqlite. max und min funktioniert auch auf den string
 select z.Buchungskreis , min(z.abfahrtsdatum), avg(z.messung) 
 from zugfahrten z 
 group by z.Buchungskreis 
 
-
+------------------------------------------------------------
 -- Welcher Buchungskreis hat die höchste Gesamtsumme bei den Zugfahrten in den jeweiligen Jahren?
 SELECT max(anz.anz_fahrten), anz.Buchungskreis from 
 (select count(distinct z.fahrtnummer) anz_fahrten, z.Buchungskreis 
 from zugfahrten z 
 group by z.Buchungskreis ) anz
 
--- Bei den verschiedenen Buchungskreisen wird von der aktuellsten Zugfahrt die Fahrzeugnummer mi tder zweithöchsten Messung gesucht
-select Fahrtnummer , Buchungskreis , max(Abfahrtszeit) from zugfahrten z
-group by Buchungskreis 
 
+------------------------------------------------------------
 -- Bei den verschiedenen Buchungskreisen wird von der aktuellsten Zugfahrt die Fahrzeugnummer mit der zweithöchsten Messung gesucht 
 -- Ranks zu Messungen 
 select m_ranks.Buchungskreis, m_ranks.Fahrzeugnummer
@@ -47,12 +68,50 @@ group by m_ranks.Buchungskreis
 having max(m_ranks.Abfahrtsdatum)
 
 
-
----- Checks 
--- Vergleiche Abfahrtsdatum mit dem Datum in der Süalte Abfahrtszeit
+------------------------------------------------------------
+---- Diverse Checks 
+-- Vergleiche Abfahrtsdatum mit dem Datum in der Spalte Abfahrtszeit
 select Fahrtnummer, Fahrzeugnummer, SUBSTR(Abfahrtsdatum,1,10) datum, SUBSTR(Abfahrtszeit,1,10) zeit from zugfahrten z
 where SUBSTR(Abfahrtsdatum,1,10) <> SUBSTR(Abfahrtszeit,1,10)   
 
 -- Ist zug id immer gleich fahrtnummer?
 select * from zugfahrten z 
 where z."Zug id" == z.Fahrtnummer 
+
+-- range der messungen - siehe zugfahrten_add_code.py für boxplot
+select min(messung), max(messung), avg(messung), max(messung) - min(messung) range
+from zugfahrten z;
+
+-- "min(messung)"	"max(messung)"	"avg(messung)"		"range"
+-- -4980.23			4999.91			2735.184631826786	9980.14
+
+-- range abfahrtsdatum
+-- hier liegt auch nahe, dass es einige fehlerhafte Daten gibt, da die Datumswerte innerhalb einer Zugfahrt bei den verschiedenen
+-- Fahrzeugen nicht einheitlich sind. Das zu korrigieren ist ohne weiteres Wissen schwierig, da man rein aufgrund der Häufigkeit eines Datums
+-- ableiten könnte welches das Richtige ist. (wenn bei 9 Fahrzeugen ein Datum steht und bei 1 ein anderes, dann ist es zwar naheliegend,
+-- dass das eine Datum falsch ist, aber ich würde es inhaltlich noch klären wollen
+select min(Abfahrtsdatum), max(Abfahrtsdatum)
+from zugfahrten z 
+
+-- visueller check, wie die Qualität der Bahnhofsnamen aussieht
+select distinct Abfahrtsbahnhof 
+from zugfahrten z 
+union
+select distinct Ankunftsbahnhof
+from zugfahrten z2 
+order by 1
+
+-- Check Durchschnittsgeschwindigkeit
+-- minmax
+-- Manche Züge sind auffallend schnell -- da passt etwas bei Entfernung oder Geschwindigkeit nicht
+
+select distinct z."Zug id" , z.Durchschnittsgeschwindigkeit, z.Abfahrtszeit, z.Ankunftszeit, z."Entfernung km" 
+from zugfahrten z 
+where z.Durchschnittsgeschwindigkeit  > 200
+order by z.Durchschnittsgeschwindigkeit  desc
+
+-- 984 Züge über 200km/h im Durchschnitt sind eine Menge - wie einheitlich ist die Distanz zwischen den gleichen Bahnhöfen
+-- für welche Bahnhöfe gibt es unterschiedilche Distanzen? -- Beide Richtungen werden ignoriert
+select count(*), z.Abfahrtsbahnhof , z.Ankunftsbahnhof , z."Entfernung km" from zugfahrten z 
+group by z.Abfahrtsbahnhof , z.Ankunftsbahnhof , z."Entfernung km" 
+order by 2,3
